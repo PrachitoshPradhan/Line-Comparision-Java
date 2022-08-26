ce281e73299c20ba40b39cc72a9ed6029094f12a
PrachitoshPradhan <prachitoshpp.pradhans@gmail.com>
2022-08-26 14:50:55 +0530

    ADDED: Comapred lengths using compareTo method.

--- src/com/lineComparision/LineComparision.java
+++ src/com/lineComparision/LineComparision.java
@@ -1,18 +1,53 @@
 package com.lineComparision;
 import com.lineComparision.Line;
+import java.lang.Double;
 import java.util.Scanner;
 
 public class LineComparision {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		double X1 = sc.nextDouble();
-		double Y1 = sc.nextDouble();
-		double X2 = sc.nextDouble();
-		double Y2 = sc.nextDouble();
+		System.out.println("x1= ");
+		Double X1 = sc.nextDouble();
+		System.out.println("y1= ");
+		Double Y1 = sc.nextDouble();
+		System.out.println("x2= ");
+		Double X2 = sc.nextDouble();
+		System.out.println("y2= ");
+		Double Y2 = sc.nextDouble();
+		
+		System.out.println("m1= ");
+		Double M1 = sc.nextDouble();
+		System.out.println("n1= ");
+		Double N1 = sc.nextDouble();
+		System.out.println("m2= ");
+		Double M2 = sc.nextDouble();
+		System.out.println("n2= ");
+		Double N2 = sc.nextDouble();
 
-		Line l = new Line(X1,Y1,X2,Y2);
-		double LineLength = l.length();
-		System.out.println("The computed length of the line is "+ LineLength);
+		Line l1 = new Line(X1,Y1,X2,Y2);
+		Line l2 = new Line(M1,N1,M2,N2);
+		
+		Double LineLength1 = l1.length();
+		Double LineLength2 = l2.length();
+		
+		Double LineCompare = (double) Double.compare(LineLength1, LineLength2);
+		
+		System.out.println("The computed length of the line is "+ LineLength1);
+		System.out.println("The computed length of the line is "+ LineLength2);
+		
+		if (LineCompare == 0)
+		{
+			System.out.println("Lengths Of Two Lines Are Equal.");
+		}
+		else if (LineCompare < 0)
+		{
+			System.out.println("Line 2 is longer than Line 1.");
+		}
+		else
+		{
+			System.out.println("Line 1 is longer than Line 2.");
+		}
+
 		sc.close();
 	}
 
